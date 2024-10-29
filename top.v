84aa34b39c51ab04071c390c7610cfb1d7b56c01
Stone <97650818+scentedrabbit@users.noreply.github.com>
2024-10-29 11:31:21 -0600

    Add files via upload

--- top.v
+++ top.v
@@ -1,50 +1,59 @@
-module top
-#(
-    parameter BIT_COUNT = 17 // Use this when passing in to your clock div!
-    // The test bench will set it appropriately
-)
-(
-    input [7:0] sw, // A and B
-    input clk, // 100 MHz board clock
-    input btnC, // Reset
-    output [3:0] an, // 7seg anodes
-    output [6:0] seg // 7seg segments
-);
-    wire [3:0] A; 
-    wire [3:0] B; 
-    wire [3:0] AplusB; 
-    wire [3:0] AminusB;
-    wire div_clock;
+module top(
+    input [3:0] sw, //sel
+    input [15:8] sw, //input bits
+    input clk,
+    input btnC,
+    output [15:0] led, // [15:8] display A value, [7:0] for display B value
+    output [6:0] seg, //display segments 
+    output [3:0] an //display anodes
+    );
     
-    assign A = sw[3:0];
-    assign B = sw[7:4]; 
-    // Instantiate the clock divider...
-    clock_divs clock(
-        .clock(clk),
-        .reset(btnC),
-        .div_clk(div_clock)
+    wire div_clock;
+    reg [7:0] A;
+    reg [7:0] B;
+    wire [7:0] Y;
+    wire [7:0] AplusB;
+    wire [7:0] AminusB;
+    wire [7:0] SHL;
+    wire [7:0] SHR;
+    wire [7:0] CMP;
+    wire [7:0] AND;
+    wire [7:0] OR;
+    wire [7:0] XOR;
+    wire [7:0] NAND;
+    wire [7:0] NOR;
+    wire [7:0] XNOR;
+    wire [7:0] INV;
+    wire [7:0] NEG;
+    
+  
+   
+    
+    
+    assign led[15:8] = A;
+    assign led[7:0] = B;
+    
+    
+     multiplexer mux(
+        .sel(sw[3:0]),
+        .ADD(AplusB),
+        .SUB(AminusB),
+        .SHL(SHL),
+        .SHR(SHR),
+        .CMP(CMP),
+        .AND(AND),
+        .OR(OR),
+        .XOR(XOR),
+        .NAND(NAND),
+        .NOR(NOR),
+        .XNOR(XNOR),
+        .INV(INV),
+        .NEG(NEG),
+        .STO(Y),
+        //.SWP,
+        .LOAD(A),
+        .data(Y)
+        
     );
-    // ... wire it up to the scanner
-    seven_seg_scanner scanner(
-        .div_clock(div_clock),
-        .reset(btnC),
-        .anode(an)
-    );
-    // ... wire the scanner to the decoder
-    seven_seg_decoder decoder(
-        .A(A),
-        .B(B),
-        .AplusB(AplusB),
-        .AminusB(AminusB),
-        .anode(an),
-        .segs(seg)
-    );
-    // Wire up the math block into the decoder
-    math_block math(
-        .A(A),
-        .B(B),
-        .AplusB(AplusB),
-        .AminusB(AminusB)
-    );
-    // Do not forget to wire up resets!!
+    
 endmodule
