09435250f8893a579da16a09bf1c68bc728b04b7
evanlelong <143737035+evanlelong@users.noreply.github.com>
2024-10-29 20:17:48 -0600

    Add files via upload

--- full_adder.v
+++ full_adder.v
@@ -8,3 +8,29 @@ module full_adder(
     assign Cout = (Sum & Cin) | (A & B);
     
 endmodule
+
+module ripple_full_adder(
+    input [7:0] A,
+    input [7:0] B,
+    input Cin,
+    output [7:0] Y,
+    output Cout
+);
+    wire [8:0] carry;
+    assign carry[0] = Cin;
+    
+    genvar i;
+    generate
+        for(i = 0; i < 8; i = i + 1) begin
+            full_adder fa(
+                .A(A[i]),
+                .B(B[i]),
+                .Cin(carry[i]),
+                .Y(Y[i]),
+                .Cout(carry[i+1])
+            );
+        end
+    endgenerate
+    
+    assign Cout = carry[8];
+endmodule
--- logic.v
+++ logic.v
@@ -1,97 +1,150 @@
-module ADD(
-    input 
-
+ module ADD(
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y,
+    output Cout
+);
+    ripple_full_adder add(
+        .A(A),
+        .B(B),
+        .Cin(1'b0),
+        .Y(Y),
+        .Cout(Cout)
     );
+
 endmodule
 
 module SUB(
-    input wire
-
+    input [7:0] A,
+    input [7:0] B,
+    output [7:0] Y,
+    output Cout
+);
+    ripple_full_adder sub(
+        .A(A),
+        .B(~B),
+        .Cin(1'b1),
+        .Y(Y),
+        .Cout(Cout)
     );
 endmodule
 
 module SHL(
-    input wire
-
-    );
+    input [7:0] A,
+    output [7:0] Y,
+);
+    assign Y = A << 1;
 endmodule
 
 module SHR(
-    input wire
-
-    );
+    input [7:0] A,
+    output [7:0] Y,
+);
+    assign Y = A >> 1;
 endmodule
 
 module CMP(
-    input wire
-
-    );
+    input [7:0] A,
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = (A == B) ? 8'b00000000 :    // Y = 0 if A == B
+               (A > B)  ? 8'b00000001 :    // Y = 1 if A > B
+                           8'b11111111; 
 endmodule
 
 module AND(
-    input wire
-
-    );
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = A & B;
 endmodule
 
 module OR(
-    input wire
-
-    );
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = A | B;
 endmodule
 
 module XOR(
-    input wire
-
-    );
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = A ^ B;
 endmodule
 
 module NAND(
-    input wire
-
-    );
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = ~(A & B);
 endmodule
 
 module NOR(
-    input wire
-
-    );
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = ~(A | B);
 endmodule
 
 module XNOR(
-    input wire
-
-    );
+    input [7:0] A, 
+    input [7:0] B,
+    output [7:0] Y
+);
+    assign Y = ~(A ^ B);
 endmodule
 
 module INV(
-    input wire
-
-    );
+    input [7:0] A,
+    output [7:0] Y
+);
+    assign Y = ~A;
 endmodule
 
 module NEG(
-    input wire
-
+    input [7:0] A,
+    input [7:0] B,
+    output [7:0] Y,
+    output Cout
+);
+    ripple_full_adder sub(
+        .A(8'b0),
+        .B(~A),
+        .Cin(1'b1),
+        .Y(Y),
+        .Cout(Cout)
     );
 endmodule
 
 module STO(
-    input wire
-
-    );
+    input [7:0] Y,
+    output [7:0] Y_out
+);
+    assign Y_out = Y;
 endmodule
 
 module SWP(
-    input wire
-
+    input [7:0] A_in,
+    input [7:0] B_in,
+    output [7:0] A_out,
+    output [7:0] B_out
     );
+    assign A_out = B_in;
+    assign B_out = A_in;
 endmodule
 
 module LOAD(
-    input wire
-
-    );
+    input [7:0] A,
+    output [7:0] Y
+);
+    assign Y = A;
 endmodule
 
 
